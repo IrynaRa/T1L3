35f365638775242e56f0bb4e5e9407d57b574895
IrynaRa <irynaradynovych@gmail.com>
2018-10-24 12:23:44 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int array[] = new int[]{2, 55, 67, 33, 12, 78};
+        int min;
+        min = array[0];
+
+        for (int i = 0; i < array.length; i++) ;
+        {
+
+            if (min>array[1]) min=array[1];
+
+            }
+            System.out.println(" ");
+        System.out.println("closest to " + min);
+
+
+
+        }
+    }
+
